d56b963d04b539456d22e342bb894437c01154db
lorex03 <lorenaflorio03@gmail.com>
2023-11-01 07:10:55 -0300

    volvi a la ruta de antes

--- Server/src/app.js
+++ Server/src/app.js
@@ -23,7 +23,7 @@ const storage = multer.memoryStorage(); // Almacenamiento en memoria (puedes cam
 const server = express();
 const corsOptions = {
 	
-	origin: "https://inmuebles360-deploy-git-main-lorenas-projects-f39fd2e6.vercel.app/", // LUEGP RUTA VERCEEL  Permite solicitudes desde este origen
+	origin: "https://inmuebles360-deploy.vercel.app", // LUEGP RUTA VERCEEL  Permite solicitudes desde este origen
 	methods: 'GET, POST, OPTIONS, PUT, DELETE',
 	allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept', // Solo permite estos encabezados
 	credentials: true, // Permite enviar cookies
